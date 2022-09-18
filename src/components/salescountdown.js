1a3133b4807b33b21b849d1184500993eeee2af8
Alesisio982019 <alessiogiovannini23@gmail.com>
2022-09-18 13:52:54 +0200

    Adjusted counter layout

--- src/components/salescountdown.js
+++ src/components/salescountdown.js
@@ -14,17 +14,17 @@ function Timer(props){
 
             <div className="row d-flex justify-content-center">
 
-              <div className="col-12 col-sm-6 col-md-3 col-xl-2">
+              <div className="col-3 col-sm-3 col-md-3 col-xl-2">
               <h3>Days</h3><h1>{days}</h1>
               </div>
-              <div className="col-12 col-sm-6 col-md-3 col-xl-2">
+              <div className="col-3 col-sm-3 col-md-3 col-xl-2">
               <h3>Hours</h3> <h1>{hours}</h1>
               </div>
-              <div className="col-12 col-sm-6 col-md-3 col-xl-2">
-              <h3>Minutes</h3><h1>{minutes}</h1>
+              <div className="col-3 col-sm-3 col-md-3 col-xl-2">
+              <h3>Min</h3><h1>{minutes}</h1>
               </div>
-              <div className="col-12 col-sm-6 col-md-3 col-xl-2">
-              <h3>Seconds</h3><h1>{seconds}</h1>
+              <div className="col-3 col-sm-3 col-md-3 col-xl-2">
+              <h3>Sec</h3><h1>{seconds}</h1>
               </div>
             
             </div>
